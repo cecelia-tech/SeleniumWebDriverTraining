31feac3b19c76851c360d68966a61653d05cf9ec
cecelia-tech <vitagriciute@gmail.com>
2022-06-06 11:46:08 +0300

    part 8 completed

---
+++ Task50/Google/SeleniumEasy/ProgressBarPage.cs
@@ -0,0 +1,37 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+
+namespace Task50Pages.SeleniumEasy;
+
+internal class ProgressBarPage : BaseClass
+{
+    const string URL = "https://demo.seleniumeasy.com/bootstrap-download-progress-demo.html";
+    const string DOWNLOAD_BUTTON_LOCATOR = "cricle-btn";
+    const string PERCENT_TEXT_LOCATOR = ".percenttext";
+    public ProgressBarPage(IWebDriver driver) : base(driver)
+    {
+        driver.Url = URL;
+    }
+
+    public ProgressBarPage? RestartPage()
+    {
+        try
+        {
+            GetElementById(DOWNLOAD_BUTTON_LOCATOR).Click();
+
+            var specifiedText = new WebDriverWait(_driver, TimeSpan.FromSeconds(15))
+                .Until(ExpectedConditions.TextToBePresentInElementLocated(By.CssSelector(PERCENT_TEXT_LOCATOR), "50%") );
+           
+            return new ProgressBarPage(_driver);
+        }
+        catch (NoSuchElementException)
+        {
+            return null;
+        }
+        catch (TimeoutException)
+        {
+            return null;
+        }
+    }
+}
---
+++ Task50/Tests/TestProgressBar.cs
@@ -0,0 +1,12 @@
+﻿namespace Tests;
+
+internal class TestProgressBar : TestBaseClass
+{
+    [Test]
+    public void TestRestartPage()
+    {
+        ProgressBarPage progressBarPage = new ProgressBarPage(_driver);
+
+        Assert.IsNotNull(progressBarPage.RestartPage(), "The page was not refreshed");
+    }
+}
