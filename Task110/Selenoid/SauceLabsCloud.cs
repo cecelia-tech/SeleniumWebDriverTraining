9a3094e1f4737d9908c0057d7f6a7893ad15b28a
cecelia-tech <vitagriciute@gmail.com>
2022-06-19 17:18:02 +0300

    Browsers working

---
+++ Task110/Selenoid/SauceLabsCloud.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using OpenQA.Selenium.Remote;
+using NUnit.Framework;
+using Google;
+
+namespace Selenoid;
+
+internal class SauceLabsCloud : TestBaseClass
+{
+    private const string SEARCH_TEXT = "Selenium";
+
+    [SetUp]
+    public void SetUp()
+    {
+        var browserOptions = SetSafari();
+
+        var sauceOptions = new Dictionary<string, object>();
+        sauceOptions.Add("username", "oauth-vitagriciute-33166");
+        sauceOptions.Add("accessKey", "c88e383d-b09e-4efa-bc5f-b165eb6ccba1");
+        browserOptions.AddAdditionalOption("sauce:options", sauceOptions);
+
+        var uri = new Uri("https://ondemand.eu-central-1.saucelabs.com:443/wd/hub");
+        _driver = new RemoteWebDriver(uri, browserOptions);
+    }
+
+    [Test]
+    public void TestBrowser()
+    {
+        HomePage homePage = new HomePage(_driver);
+
+        try
+        {
+            Assert.IsTrue(homePage.IsLoaded(), "Home page was not loaded");
+            var searchPage = homePage.Search(SEARCH_TEXT);
+            Assert.IsTrue(searchPage.IsLoaded(), "Search page was not loaded");
+        }
+        catch (Exception e)
+        {
+            Assert.Fail(e.Message);
+        }
+    }
+}
---
+++ Task110/Selenoid/TestBaseClass.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Safari;
+
+namespace Selenoid;
+
+internal class TestBaseClass
+{
+    protected IWebDriver _driver;
+
+    internal EdgeOptions SetEdge()
+    {
+        var browserOptions = new EdgeOptions();
+        browserOptions.PlatformName = "Windows 10";
+        browserOptions.BrowserVersion = "latest";
+        return browserOptions;
+    }
+
+    internal FirefoxOptions SetFireFox()
+    {
+        var browserOptions = new FirefoxOptions();
+        browserOptions.PlatformName = "Windows 8.1";
+        browserOptions.BrowserVersion = "latest";
+        return browserOptions;
+    }
+
+    internal SafariOptions SetSafari()
+    {
+        var browserOptions = new SafariOptions();
+        browserOptions.PlatformName = "macOS 10.15";
+        browserOptions.BrowserVersion = "latest";
+        return browserOptions;
+    }
+
+    [TearDown]
+    public void CleanUp()
+    {
+        _driver.Quit();
+    }
+}
